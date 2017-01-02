eb25cbdfc2b37fe1881b3ec3e64dfbba8608c0bd
Stefan Mirea <steven.mirea@gmail.com>
2017-01-02 22:52:11 +0200

    Fixed colors

--- src/inject.js
+++ src/inject.js
@@ -30,12 +30,12 @@ const init = () => {
 
     const {matrix, colors} = generatePreview(imgData);
 
-    const matrixData = matrix.map(arr => arr.map(hsv2string));
+    const matrixData = matrix.map(arr => arr.map(hsl2string));
     localStorage.lastMatrix = JSON.stringify(matrixData);
     const logic = window.logic = new Logic({
         width: COLS,
         height: ROWS,
-        colors: colors.map(hsv2string),
+        colors: colors.map(hsl2string),
         data: matrixData,
     });
 
@@ -47,7 +47,7 @@ const init = () => {
     );
 };
 
-const hsv2string = ({h, s, v}) => `hsl(${h}, ${s}%, ${v}%)`;
+const hsl2string = ({h, s, l}) => `hsl(${h}, ${s}%, ${l}%)`;
 
 const $ = el => Object.assign(el, {
     attr: obj => {
@@ -141,7 +141,7 @@ const generatePreview = imgData => {
                 (x + 0.5) * cellW,
                 (y + 0.5) * cellH, 1, 1
             );
-            matrix[x][y] = rgb2hsv(r, g, b);
+            matrix[x][y] = rgbToHsl(r, g, b);
             colors[matrix[x][y].h] = matrix[x][y];
 
             // const {h, s, v} = matrix[x][y];
@@ -177,44 +177,29 @@ const getImageData = () => {
     return tmpCtx.getImageData(0, delta, tmpCanvas.width, tmpCanvas.height - delta * 2);
 };
 
-const rgb2hsv = (a, b, c) => {
-    var rr, gg, bb,
-        r = a / 255,
-        g = b / 255,
-        b = c / 255,
-        h, s,
-        v = Math.max(r, g, b),
-        diff = v - Math.min(r, g, b),
-        diffc = function(c){
-            return (v - c) / 6 / diff + 1 / 2;
-        };
+const rgbToHsl = (r, g, b) => {
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
 
-    if (diff == 0) {
-        h = s = 0;
-    } else {
-        s = diff / v;
-        rr = diffc(r);
-        gg = diffc(g);
-        bb = diffc(b);
-
-        if (r === v) {
-            h = bb - gg;
-        }else if (g === v) {
-            h = (1 / 3) + rr - bb;
-        }else if (b === v) {
-            h = (2 / 3) + gg - rr;
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
         }
-        if (h < 0) {
-            h += 1;
-        }else if (h > 1) {
-            h -= 1;
-        }
+        h /= 6;
     }
+
     return {
         h: Math.round(h * 360),
         s: Math.round(s * 100),
-        v: Math.round(v * 100)
+        l: Math.round(l * 100),
     };
-};
+}
 
 init();
